22adb351f3d0c6d43edb95affbfc224fb33aa7ab
sahalmahin <samirsatter0142@gmail.com>
2023-12-12 20:55:46 +0600

    Axios and jsonwebtoken are commonly integrated in server site

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.port || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
-console.log();
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dygd3dy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,6 +26,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = async (req, res, next) => {
+    console.log('called:', req.host, req.originalUrl);
+    next();
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token);
+    if (!token) {
+        return res.status(401).send({ message: 'not authorized' });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        // err
+        if (err) {
+            console.log(err);
+            return res.status(401).send({ message: 'unauthorized' });
+        }
+        // if token is valid then it would be decoded
+        console.log('value in the token', decoded);
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -32,7 +60,23 @@ async function run() {
         // bookings of "carDoctor"
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
-        app.get('/services', async (req, res) => {
+        // auth related api
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log(user);
+
+            // token
+            const token = jwt.sign(user,
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false, // http://localhost:5173/login
+            }).send({ success: true });
+        })
+
+        // services related api
+        app.get('/services', logger, async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -41,23 +85,28 @@ async function run() {
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-
             const options = {
-                // Include only the `title` `price` `img` and `service_id` fields in the returned document
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
-
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
 
         // bookings
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
             console.log(req.query.email);
+            // console.log('token got here', req.cookies.token);
+            console.log('user in the valid token', req.user);
+
+            if (req.query.email !== req.user.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email };
             }
+            
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
@@ -76,7 +125,7 @@ async function run() {
             console.log(updatedBooking);
             const updateDoc = {
                 $set: {
-                    status: updatedBooking.status
+                    status: updatedBooking.status // send this status from client site
                 }
             }
             const result = await bookingCollection.updateOne(filter, updateDoc);
